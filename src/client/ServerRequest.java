177248c14d23cd85b5adb55ece14f98d2226d1f7
oliverkirkegaard <oliver.a.g.kirkegaard@gmail.com>
2022-03-17 10:00:44 +0100

    Change from static to normal added socket

--- src/client/ServerRequest.java
+++ src/client/ServerRequest.java
@@ -2,8 +2,30 @@ package client;
 
 import model.ResponseStatus;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
 public class ServerRequest {
-    public static ResponseStatus connect(String playerName){
+    private final Socket connectionSocket;
+
+    public ServerRequest() throws IOException {
+        this.connectionSocket = new Socket("localhost", 6789);
+    }
+
+    public ResponseStatus connect(String playerName){
         return null;
+
+    }
+    public void performRequest(String data) {
+
+            try {
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                outToClient.writeBytes(data + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 }
