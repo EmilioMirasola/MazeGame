84bbad2a6ca988a3904bfa2569ceec4e2465bbdb
Emilio Mirasola <emilio@mirasola.dk>
2022-03-21 11:09:11 +0100

    Stream initializing

--- src/server/ServerReadThread.java
+++ src/server/ServerReadThread.java
@@ -12,11 +12,12 @@ import java.net.Socket;
 public class ServerReadThread extends Thread {
 	private Socket connectionSocket;
 	private Controller controller;
-	private DataOutputStream dataOutputStream = new DataOutputStream(this.getConnectionSocket().getOutputStream());
+	private DataOutputStream dataOutputStream;
 
 	public ServerReadThread(Socket connectionSocket) throws IOException {
 		this.connectionSocket = connectionSocket;
 		this.controller = new Controller();
+		this.dataOutputStream = new DataOutputStream(connectionSocket.getOutputStream());
 	}
 
 	public void run() {
